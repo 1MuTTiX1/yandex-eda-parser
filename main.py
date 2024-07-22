db94e7646c8292ab55196f14c58655aba5d7ae92
1MuTTiX1 <149588467+1MuTTiX1@users.noreply.github.com>
2024-07-22 19:18:06 +0400

    Update main.py
    Произошли изменения на сайте Яндекс и теперь элементы удалятся во время скролла.Парсер был починен и добавлено что ищет по определённому времени(завтра 15:30).

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
-
 fireFoxOptions = webdriver.FirefoxOptions()
 fireFoxOptions.add_argument("--window-size=1920,1080")
 fireFoxOptions.add_argument('--start-maximized')
@@ -15,63 +14,74 @@ fireFoxOptions.add_argument("--headless")
 driver = webdriver.Firefox(options=fireFoxOptions)
 url = 'https://eats.yandex.com/ru-am/Yerevan?shippingType=delivery'
 try:
-
-#Функция принимает адреса в качестве аргумента и выдаёт список Url адресов всех ресторанов в txt для дальнейшей обработки    
     def colector_url(address):
         driver.maximize_window()
         driver.get(url=url)
         time.sleep(3)
 
-#Блок по вводу интересуещего адреса
+#Блок по вводу интересуещего адреса и выборавравренмена на завтра 15:00
 
         clickable_search = driver.find_element(By.CLASS_NAME, 'DesktopAddressButton_address')
         ActionChains(driver).click(clickable_search).perform()
         time.sleep(1)
         write = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/div/div[1]/div[2]/div/div/div[1]/input')
         write.send_keys(address)
-        time.sleep(3)
+        time.sleep(1)
         write.send_keys(Keys.SPACE)
-        time.sleep(3)
+        time.sleep(2)
         clickable_place = driver.find_element(By.ID, 'react-autowhatever-1--item-0')
         ActionChains(driver).click(clickable_place).perform()
-        time.sleep(2)
+        time.sleep(1)
         ok = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/div/div[1]/div[2]/button')
         ActionChains(driver).click(ok).perform()
+        time.sleep(3)
+        click_time = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[1]/main/div/div/main/div[4]/div/div')
+        ActionChains(driver).click(click_time).perform()
+        time.sleep(1)
+        click_day = driver.find_element(By.XPATH,'/html/body/div[3]/div/div/div/div/div/div[1]/button[2]')
+        ActionChains(driver).click(click_day).perform()
+        time.sleep(1)
+        time_click = driver.find_element(By.XPATH,'/html/body/div[3]/div/div/div/div/div/div[2]/button[31]')
+        driver.execute_script("arguments[0].scrollIntoView();", time_click)
+        ActionChains(driver).click(time_click).perform()
+        time.sleep(3)
 
-#Блок скролла страницы если есть вариант более производительней пишите(если знаете как сделать скрол через selenium и более оптимизированно, жду откликов)
+
+#Блок скролла страницы и поиска Url ресторана
         a = 0
+        b = 0
+        url_list = []
         while True:
             driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.CONTROL + Keys.END)
-            time.sleep(4)
+            time.sleep(2)
+
             soup = BeautifulSoup(driver.page_source, 'lxml')
             find = soup.find_all('div', 'PlaceListBduItem_placesListItem PlaceListBduItem_lg')
+            for i in find:
+                if i.find('a'):
+                    ur = f'https://eats.yandex.com{i.find('a').get('href')}'
+                    url_list.append(ur)
             if a == len(find):
                 print(f'Собрал: {a}')
                 a = 0
                 break
             else:
                 a = len(find)
-        time.sleep(5)
 
+#Запись URL
 
-#Поиск URL
-        carts = soup.find_all('div', 'PlaceListBduItem_placesListItem PlaceListBduItem_lg')
-        url_list = []
-        for i in carts:
-            ur = f'https://eats.yandex.com{i.find('a').get('href')}'
-            url_list.append(ur)
+        url_clear = list(set(url_list))
 
-#Запись URL(Если наете как более красово сделать запись со спец знаками и более оптимизировано жду отзывов)
-        with open(f'{address.replace(',','').replace('/','_')}.txt','w',encoding="utf-8") as f:
-            for urls in url_list:
+        with open(f'F:/sql_base/eda_yandex_url/{address.replace(',','').replace('/','_')}.txt','w',encoding="utf-8") as f:
+            for urls in url_clear:
                 f.write(f'{urls}\n')
 
-
-
-
+#Запуск функции
+    colector_url('Ваш адрес')
 
 except Exception as ex:
     print(ex)
 finally:
     driver.close()
     driver.quit()
+
